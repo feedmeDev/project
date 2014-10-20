e40a22bb4da5552316e814e2d4af18e9ff810077
feedmeDev <feedmedev@gmail.com>
2014-10-20 13:57:04 +1300

    added rails to folder

---
+++ feedme/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Feedme::Application.config.secret_key_base = 'dfb6b363f3e2a9a29aa1b5bf44e0a2ec32dfd3847b8e3f7b159fd58a108e1b6347a50bebb7fd6c603bd86923abfa6280a918ce892ee7124c3b19f309910efa53'
