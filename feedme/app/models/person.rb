3e78852658e241b2b39740644143cb3609f6b7c9
Ubuntu <ubuntu@ip-172-31-7-246.ap-southeast-2.compute.internal>
2014-11-03 11:31:23 +1300

    hopefully made the passwords encrypted, testing to follow

--- feedme/app/models/person.rb
+++ feedme/app/models/person.rb
@@ -1,2 +1,40 @@
+require 'bcrypt'
+
 class Person < ActiveRecord::Base
+
+	attr_accessor :hash_new_password, :new_password_confirmation
+
+	validates_confirmation_of :new_password, if=>:password_changed?
+
+	before_save :hash_new_password, :if=>:password_changed?
+
+	def password_changed?
+		!@new_password.blank?
+	end
+
+	private
+	# This is where the real work is done, store the BCrypt has in the
+	# database
+	def hash_new_password
+		self.hashed_password = BCrypt::Password.create(@new_password)
+	end
+
+	# As is the 'standard' with rails apps we'll return the user record if the
+    # password is correct and nil if it isn't.
+    def self.authenticate(username, password)
+      # Because we use bcrypt we can't do this query in one part, first
+      # we need to fetch the potential user
+      if user = find_by_email(username)
+        # Then compare the provided password against the hashed one in the db.
+        if BCrypt::Password.new(user.hashed_password).is_password? password
+          # If they match we return the user 
+          return user
+        end
+      end
+      # If we get here it means either there's no user with that email, or the wrong
+      # password was provided.  But we don't want to let an attacker know which. 
+      return nil
+    end
+
+
 end
