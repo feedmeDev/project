0dc7459d1a2604ce1c21c169e7ef11ad39e83445
feedmeDev <feedmeDev@gmail.com>
2014-11-10 15:09:03 +1300

    added more tests

--- testing/person/add_staff.sh
+++ testing/person/add_staff.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -i -H "Content-Type: application/json" -X POST localhost:3000/staff.json -d '{"person": {"name":"test person 1", "username":"testii", "password":"pass1"}}'
+curl -i -H "Content-Type: application/json" -X POST localhost:3000/staff.json -d '{"person": {"name":"test person 1", "username":"testii", "password":"pass1word"}}'
 
--- testing/person/add_student.sh
+++ testing/person/add_student.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl -i -H "Content-Type: application/json" -X POST localhost:3000/student.json -d '{"person": {"name":"test student 1", "username":"testiiStudent", "password":"ass1"}}'
+curl -i -H "Content-Type: application/json" -X POST localhost:3000/student.json -d '{"person": {"name":"test student 1", "username":"testiiStudent", "password":"pass1word"}}'
+
+
